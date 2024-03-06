66db24987c5b25bedf5302684cbdf71ba9ed206e
Mialoyto <mialoyto1999@gmail.com>
2024-03-06 05:29:51 -0500

    JP-PRODUCIONES

---
+++ interface.py
@@ -0,0 +1,14 @@
+from tkinter import*
+
+
+raiz=Tk()
+
+raiz.title("Control de Inventario")
+
+#raiz.resizable(True,True)  => ajustar el tamaño (Ancho , Alto)
+
+#raiz.iconbitmap("nombre.ico")
+
+raiz.geometry("800x600")  # (Ancho, Alto)
+
+raiz.mainloop()
